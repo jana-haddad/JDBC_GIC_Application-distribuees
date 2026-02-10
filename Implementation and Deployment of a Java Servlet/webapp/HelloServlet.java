f9ef709d50970ac0e2d0c0bb974802fe2b9da312
jana-haddad <jana.haddad2@net.usj.edu.lb>
2026-02-10 13:49:01 +0200

    Add files via upload

---
+++ Implementation and Deployment of a Java Servlet/webapp/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.usj;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello Jana Haddad</h1>");
+    }
+}
